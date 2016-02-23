b351bfcdd47e2c6361b27580f6d57616e46238de
pranshunz <pranshu@stringtheory.me>
2016-02-23 21:05:32 +1300

    Show claims upon login

--- Analystick.Web/src/Analystick.Web/Controllers/HomeController.cs
+++ Analystick.Web/src/Analystick.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNet.Authorization;
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,7 +14,9 @@ namespace Analystick.Web.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var claims = User.Claims.Select(c => new {type = c.Type, value = c.Value}).OrderBy(c => c.type);
+            var model = (object) JsonConvert.SerializeObject(claims, Formatting.Indented);
+            return View(model);
         }
     }
 }
